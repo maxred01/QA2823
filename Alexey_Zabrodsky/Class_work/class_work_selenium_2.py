afb7fbcd7be07765fa66dd39bcb60be02a8e94a8
AlexeyZabrodsky <LeshaZabrodsky@gmail.com>
2023-11-16 22:00:43 +0300

    Class_work_selenium_2_new

--- Alexey_Zabrodsky/Class_work/class_work_selenium_2.py
+++ Alexey_Zabrodsky/Class_work/class_work_selenium_2.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-
 # option = Options()
 # option.add_argument("--window-size=2000,800") #разрешение экрана
 # driver = webdriver.Chrome(options=option)
@@ -15,16 +14,19 @@ driver.get("https://hoster.by/")
 driver.find_element(By.CSS_SELECTOR, 'input.m-input.m-b1').send_keys('Волшебство')
 driver.implicitly_wait(5)
 
-
 driver.find_element(By.CSS_SELECTOR, 'button.m-button.red').click()
 driver.implicitly_wait(5)
 #
-# driver.find_element(By.XPATH, '//input[@class="auth-input auth-input_primary auth-input_base auth-form__input '
-#                               'auth-form__input_width_full"]').clear()
-# driver.implicitly_wait(5)
+driver.find_element(By.XPATH, '//input[@class="auth-input auth-input_primary '
+                              'auth-input_base auth-form__input '
+                              'auth-form__input_width_full"]').clear()
+driver.implicitly_wait(5)
 
-# element = driver.find_element(By.XPATH, '//input[@class="auth-input auth-input_primary auth-input_base '
-#                                         'auth-form__input auth-form__input_width_full"]').get_attribute('placeholder')
+element = driver.find_element(By.XPATH, '//input[@class="auth-input '
+                                        'auth-input_primary auth-input_base '
+                                        'auth-form__input '
+                                        'auth-form__input_width_full"]') \
+    .get_attribute('placeholder')
 # print(element)
 
 driver.find_element(By.CSS_SELECTOR, 'button#accept').click()
@@ -62,8 +64,9 @@ time.sleep(1)
 driver.find_element(By.CSS_SELECTOR, 'button#domain_org').click()
 # driver.implicitly_wait(5)
 time.sleep(1)
-element = driver.find_element(By.CSS_SELECTOR, 'button#domain').is_displayed()
-assert element == True
+
+# element = driver.find_element(By.CSS_SELECTOR, 'button#domain').is_displayed()
+# assert element == True
 
 # driver.execute_script("""window.open('https://www.google.com/')""")
 # driver.implicitly_wait(5)
