91e1978be6a22de120af5011f7b4118be711bcd4
deruzhka <arina2002470@gmail.com>
2023-11-16 16:05:23 +0300

    homework selenium

---
+++ Arina_Derugo/homework/homework4.py
@@ -0,0 +1,141 @@
+# 1
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+#
+#
+# driver = webdriver.Chrome()
+# driver.get('https://www.google.com/')
+# keyword = 'geeksforgeeks'
+#
+# element = driver.find_element(By.XPATH, "//*[@id='SIvCob']").text
+# print(element)
+# print(driver.title)
+# print(driver.current_url)
+# print(driver.page_source)
+
+
+# 2 ошибку выдает :(
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+#
+#
+# driver = webdriver.Chrome()
+# driver.get('https://www.youtube.com/')
+# keyword = 'geeksforgeeks'
+#
+# element = driver.find_element(By.CSS_SELECTOR, "[class*='title-text style-scope ytd-display-ad-renderer']").text
+# print(element)
+# print(driver.title)
+# print(driver.current_url)
+# print(driver.page_source)
+
+
+# 3
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+#
+#
+# driver = webdriver.Chrome()
+# driver.get('https://www.facebook.com/')
+# keyword = 'geeksforgeeks'
+#
+# element = driver.find_element(By.CSS_SELECTOR, "[class*='_8eso']").text
+# print(element)
+# print(driver.title)
+# print(driver.current_url)
+# print(driver.page_source)
+
+
+# 4
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+#
+#
+# driver = webdriver.Chrome()
+# driver.get('https://www.instagram.com/')
+# keyword = 'geeksforgeeks'
+#
+# element = driver.find_element(By.CSS_SELECTOR, "[class*='_ab25']").text
+# print(element)
+# print(driver.title)
+# print(driver.current_url)
+# print(driver.page_source)
+
+# 5 не получилось
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+#
+#
+# driver = webdriver.Chrome()
+# driver.get('https://twitter.com/')
+# keyword = 'geeksforgeeks'
+#
+# element = driver.find_element(By.XPATH, "//*[text()='Join today.']").text
+# print(element)
+# print(driver.title)
+# print(driver.current_url)
+# print(driver.page_source)
+
+# 6 не получилось(((
+
+# 7
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+#
+#
+# driver = webdriver.Chrome()
+# driver.get('https://www.wikipedia.org/')
+# keyword = 'geeksforgeeks'
+#
+# element = driver.find_element(By.CSS_SELECTOR, "[class*='jsl10n localized-slogan']").text
+# print(element)
+# print(driver.title)
+# print(driver.current_url)
+# print(driver.page_source)
+
+# 8
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+#
+#
+# driver = webdriver.Chrome()
+# driver.get('https://yahoo.com/')
+# keyword = 'geeksforgeeks'
+#
+# element = driver.find_element(By.CSS_SELECTOR, "[class*='_yb_io0r2 _yb_1hru9 _yb_1jh73 _yb_1gfxm']").text
+# print(element)
+# print(driver.title)
+# print(driver.current_url)
+# print(driver.page_source)
+
+#9 не получилось, сразу открывается проверка что не робот
+
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.chrome.options import Options
+#
+#
+# driver = webdriver.Chrome()
+# driver.get('https://yandex.ru/')
+# keyword = 'geeksforgeeks'
+#
+# element = driver.find_element(By.CSS_SELECTOR, "[class*='card-news-story__text-3F']").text
+# print(element)
+# print(driver.title)
+# print(driver.current_url)
+# print(driver.page_source)
+
