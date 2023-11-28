94e00e08de672b99f6de3dd7e029c3fbb0f9db69
ven <tail.90@list.ru>
2023-11-28 18:05:10 +0300

    homework5 done

--- Mikhnovich_Aliaksei/Homework/Homework_2/home_work_2.py
+++ Mikhnovich_Aliaksei/Homework/Homework_2/home_work_2.py
@@ -1,8 +1,8 @@
 # https://maxkorzh.live/ - music tab
-"""//div[@class="menu tablet_hide mobile_hide"]//*[@href="muzyika/"]"""
+MUS_TAB = '//div[@class="menu tablet_hide mobile_hide"]//*[@href="muzyika/"]'
 # https://www.fpp.net/work/ https://share.cleanshot.com/LZDmbQkZ - search num '2'
-"""//*[@class="relative w-full"]//*[text()="2"]"""
+NUM_2 = '//*[@class="relative w-full"]//*[text()="2"]'
 # https://www.awwwards.com/ https://share.cleanshot.com/y44tLHBP - button 404
-"""a[href$="/websites/404-pages/"]"""
+BUT_404 = 'a[href$="/websites/404-pages/"]'
 # https: // www.pttrns.com / https: // share.cleanshot.com / cdv5jCzf - trial button
-"""[class="gspb-buttonbox-title"]"""
+TRIAL_BUTTON = '[class="gspb-buttonbox-title"]'
