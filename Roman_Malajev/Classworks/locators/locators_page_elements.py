66dc6968b8dd573e704b0a188455956306bf7d30
co11apz <romanmalajev@gmail.com>
2023-11-28 21:55:08 +0300

    fix

---
+++ Roman_Malajev/Classworks/locators/locators_page_elements.py
@@ -0,0 +1,5 @@
+class LocatorsForms:
+    Full_Name = '[id="userName"]'
+    Email = '[id="userEmail"]'
+    Current_Address = '[id="currentAddress"]'
+    Permanent_Address = '[id="permanentAddress"]'
