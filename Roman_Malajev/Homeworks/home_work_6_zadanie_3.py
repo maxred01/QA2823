6958bb7332e12d44a016349f456b9d74c5174e7c
roman <romanmalajev@gmail.com>
2023-11-30 10:01:52 +0300

    homework6_completed

--- Roman_Malajev/Homeworks/home_work_6_zadanie_3.py
+++ Roman_Malajev/Homeworks/home_work_6_zadanie_3.py
@@ -5,13 +5,13 @@ from selenium.webdriver.common.by import By
 import pytest_check as check
 
 
-created = 'https://demoqa.com/created'
-no_content = 'https://demoqa.com/no-content'
-moved = 'https://demoqa.com/moved'
-bad_request = 'https://demoqa.com/bad-request'
-unauthorized = 'https://demoqa.com/unauthorized'
-forbidden = 'https://demoqa.com/forbidden'
-not_found = 'https://demoqa.com/invalid-url'
+CREATED = 'https://demoqa.com/created'
+NO_CONTENT = 'https://demoqa.com/no-content'
+MOVED = 'https://demoqa.com/moved'
+BAD_REQUEST = 'https://demoqa.com/bad-request'
+UNAUTHORIZED = 'https://demoqa.com/unauthorized'
+FORBIDDEN = 'https://demoqa.com/forbidden'
+NOT_FOUND = 'https://demoqa.com/invalid-url'
 
 
 def test_homework6_check_links():
@@ -24,7 +24,7 @@ def test_homework6_check_links():
     driver.implicitly_wait(5)
     driver.execute_script('window.scrollBy(0,300)')
 
-    links_url = [created, no_content, moved, bad_request, unauthorized, forbidden, not_found]
+    links_url = [CREATED, NO_CONTENT, MOVED, BAD_REQUEST, UNAUTHORIZED, FORBIDDEN, NOT_FOUND]
     links_url_data = {}
 
     driver.implicitly_wait(5)
@@ -42,10 +42,19 @@ def test_homework6_check_links():
         time.sleep(0.5)
         value = driver.find_element(By.XPATH, '//p[@id="linkResponse"]//b[1]').text
         links_data['status_expected'] = int(value)
-        links_data['reason_expected'] = driver.find_element(By.XPATH, '//p[@id="linkResponse"]//b[2]').text
+        links_data['reason_expected'] = driver.find_element \
+            (By.XPATH, '//p[@id="linkResponse"]//b[2]').text
         time.sleep(0.5)
 
-        check.equal(links_url_data['status_actual'], links_data['status_expected'], 'Статус отличается')
-        check.equal(links_url_data['reason_actual'], links_data['reason_expected'], 'Описание статуса отличается')
+        check.equal(
+            links_url_data['status_actual'],
+            links_data['status_expected'],
+            'Статус отличается'
+        )
+        check.equal(
+            links_url_data['reason_actual'],
+            links_data['reason_expected'],
+            'Описание статуса отличается'
+        )
 
     driver.close()
