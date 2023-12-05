3cfb8031edbbce1aa78867dc91333f15b31deb56
Tatsiana Malahova <tanyamalahova1919@mail.ru>
2023-12-05 19:22:14 +0300

    add diploma

---
+++ Tatyana_Malahova/diploma/pages/locators/autorization_page_locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+e_mail_address = (By.CSS_SELECTOR, "//input[@id='login.email']")
+password = (By.CSS_SELECTOR, "//input[@id='login.secret']")
+log_in = (
+By.CSS_SELECTOR, "//button[@class='DJxzzA u9KIT8 uEg2FS U_OhzR ZkIJC- Vn-7c- FCIprz heWLCX _9K5FC9 LyRfpJ R7mUGT /" \
+                 "Md_Vex NN8L-8 h14nQ_ tiE3Mh _5PMpaO EKabf7 aX2-iv r9BRio mo6ZnF Wy3rmK']")
+register = (By.CSS_SELECTOR, "//button[@class='DJxzzA u9KIT8 uEg2FS U_OhzR ZkIJC- Vn-7c- FCIprz heWLCX _9K5FC9 LyRfpJ /" \
+                             "R7mUGT NN8L-8 K82if3 mVyMPj Md_Vex tiE3Mh A7pNov _4F506m CkamMo EKabf7 aX2-iv r9BRio mo6ZnF J5vq7v Wy3rmK']")
