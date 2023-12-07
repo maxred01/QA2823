86c1f56868bcb566f4752cfdc5266e3922ac4285
AlexeyZabrodsky <LeshaZabrodsky@gmail.com>
2023-12-07 14:10:47 +0300

    fix

--- Alexey_Zabrodsky/diplom/pages/base_page.py
+++ Alexey_Zabrodsky/diplom/pages/base_page.py
@@ -101,3 +101,52 @@ class BasePage:
         """ Метод get_current_url() возвращает текущий URL-адрес страницы"""
 
         return self.driver.current_url
+
+    def is_elements_text_equal_to(self, *args, element_text):
+        """ Метод is_elements_text_equal_to() проверяет,
+            что текст элемента соответствует заданному значению.
+            Метод принимает два аргумента: by_name и by_val,
+            которые определяют, как искать элемент на странице,
+            и element_text - текст, который должен быть найден в элементе.
+            Метод использует WebDriverWait для ожидания
+            появления элемента на странице в течение 10 секунд.
+            Если текст элемента соответствует заданному значению,
+            метод возвращает True. Если текст элемента не соответствует
+            заданному значению, возникает исключение TimeoutException,
+            и метод возвращает False"""
+
+        try:
+            by_name, by_val = args[0]
+            WebDriverWait(self.driver, 10).until(
+                EC.text_to_be_present_in_element((by_name, by_val), element_text),
+            )
+        except TimeoutException:
+            return False
+        return True
+
+    def drag_and_drop_from_right_to_left(self, source, x_offset=0, y_offset=0):
+        """ Метод drag_and_drop_from_right_to_left() выполняет перетаскивание
+            элемента справа налево. Метод принимает два аргумента: source - элемент,
+            который нужно перетащить, и x_offset, y_offset - смещение на которое
+            нужно перетащить элемент. Метод использует метод find_element() для
+            поиска элемента на странице, создает объект ActionChains, который
+            используется для выполнения перетаскивания элемента. x_offset и y_offset
+            используются для указания смещения, на которое нужно перетащить элемент.
+            Если не указано смещение, элемент будет перетаскиваться на ту же позицию,
+            где он находится на странице"""
+
+        source_web_element = self.find_element(source)
+        action = ActionChains(self.driver)
+        action.drag_and_drop_by_offset(source_web_element, x_offset, y_offset).perform()
+
+    def check_for_url_is_changed(self, current_url, url_that_should_be):
+        """ Метод check_for_url_is_changed() проверяет,
+            что URL-адрес страницы изменился. Метод принимает два аргумента: current_url
+            - текущий URL-адрес страницы и url_that_should_be - URL-адрес,
+            который должен быть после выполнения действия. Метод сравнивает
+            текущий URL-адрес страницы с ожидаемым URL-адресом. Если текущий
+            URL-адрес отличается от ожидаемого, возникает исключение AssertionError.
+            Если URL-адрес соответствует ожидаемому, метод ничего не возвращает"""
+
+        with allure.step(f"Проверяем, что текущий url равен {url_that_should_be}"):
+            assert current_url == url_that_should_be
