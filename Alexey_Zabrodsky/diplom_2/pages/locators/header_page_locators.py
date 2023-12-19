5eb1e05a679f4e2a547ed1a4d97d91fcf24d245f
AlexeyZabrodsky <LeshaZabrodsky@gmail.com>
2023-12-19 21:56:43 +0300

    fix

--- Alexey_Zabrodsky/diplom_2/pages/locators/header_page_locators.py
+++ Alexey_Zabrodsky/diplom_2/pages/locators/header_page_locators.py
@@ -12,13 +12,28 @@ class HeaderBtn(WebPage):
 
         super().__init__(web_driver, url)
 
-    # О магазине
+# <--Header-->
     button_o_magazine = WebElement(css_selector='div.header__menu__item:nth-of-type(1) a')
-    # Доставка и оплата
     button_dostavka_i_oplata = WebElement(css_selector='div.header__menu__item:nth-of-type(2) a')
-    # Гарантия
     button_garantiya = WebElement(css_selector='div.header__menu__item:nth-of-type(3) a')
-    # Инстаграм
     button_instagram = WebElement(css_selector='div.header div.header__social_links a:nth-of-type(1)')
-    # Фейсбук
     button_facebook = WebElement(css_selector='div.header div.header__social_links a:nth-of-type(2)')
+    button_vk = WebElement(css_selector='div.header div.header__social_links a:nth-of-type(3)')
+    button_logo = WebElement(xpath='//div[@class="header__logo__image"]/a')
+    button_korzina = WebElement(css_selector='div.header__wrapper a#id__header__shopcart__title__link')
+    input_poisk = WebElement(css_selector='div.header__search input.top-search-input')
+    button_poisk = WebElement(css_selector='div.header__search button#id__top__search__button')
+    button_viber = WebElement(css_selector='div.header div.block__phone__social__item.viber a')
+    button_watsapp = WebElement(css_selector='div.header div.block__phone__social__item.watsapp a')
+    button_telegram = WebElement(css_selector='div.header div.block__phone__social__item.telegram a')
+    number_velcom = WebElement(css_selector='div.header a#id__header_phone_velcom')
+    number_mts = WebElement(css_selector='div.header a#id__header_phone_mts')
+    number_city = WebElement(css_selector='div.header a#id__header_phone_city')
+
+# <--mini-header-->
+    button_korzina_mini = WebElement(css_selector='div.mobile__navbar.active '
+                                                  'a#id__header__shopcart__title__link')
+    input_poisk_mini = WebElement(css_selector='div.mobile__navbar__search input.top-search-input')
+    button_poisk_mini = WebElement(css_selector='div.mobile__navbar.active '
+                                                'button#id__top__search__button')
+    button_logo_mini = WebElement(css_selector='div.mobile__navbar__logo a')
--- Alexey_Zabrodsky/diplom_2/tests/test_futer.py
+++ Alexey_Zabrodsky/diplom_2/tests/test_futer.py
@@ -14,42 +14,113 @@ def test_header_btn_1(web_browser):
 
     page = HeaderBtn(web_browser)
 
-    # with allure.step("Проверка кнопки 'О магазине'"):
-    #     if check.equal(page.button_o_magazine.get_text(), 'О магазине'):
-    #         check.equal(page.button_o_magazine.get_attribute('href'), 'https://www.dollar.by/about')
-    #         page.button_o_magazine.click(1)
-    #         page1 = page.get_current_url()
-    #         check.equal(page1, page.get_current_url())
-    #         check.is_true(page.button_o_magazine.wait_for_visibility())
-    #         check.is_true(page.button_o_magazine.wait_to_be_clickable())
-    #
-    # with allure.step("Проверка кнопки 'Доставка и оплата'"):
-    #     if check.equal(page.button_dostavka_i_oplata.get_text(), 'Доставка и оплата'):
-    #         check.equal(page.button_dostavka_i_oplata.get_attribute('href'), 'https://www.dollar.by/orders')
-    #         page.button_dostavka_i_oplata.click(1)
-    #         page1 = page.get_current_url()
-    #         check.equal(page1, page.get_current_url())
-    #         check.is_true(page.button_dostavka_i_oplata.wait_for_visibility())
-    #         check.is_true(page.button_dostavka_i_oplata.wait_to_be_clickable())
+    with allure.step("Проверка кнопки 'О магазине'"):
+        if check.equal(page.button_o_magazine.get_text(), 'О магазине'):
+            check.equal(page.button_o_magazine.get_attribute('href'), 'https://www.dollar.by/about')
+            page.button_o_magazine.click(1)
+            page1 = page.get_current_url()
+            check.equal(page1, 'https://www.dollar.by/about')
+            check.is_true(page.button_o_magazine.wait_for_visibility())
+            check.is_true(page.button_o_magazine.wait_to_be_clickable())
 
-    # with allure.step("Проверка кнопки 'Гарантия'"):
-    #     if check.equal(page.button_garantiya.get_text(), 'Гарантия'):
-    #         check.equal(page.button_garantiya.get_attribute('href'), 'https://www.dollar.by/garantiya')
-    #         page.button_garantiya.click(1)
-    #         page1 = page.get_current_url()
-    #         check.equal(page1, page.get_current_url())
-    #         check.is_true(page.button_garantiya.wait_for_visibility())
-    #         check.is_true(page.button_garantiya.wait_to_be_clickable())
+    with allure.step("Проверка кнопки 'Доставка и оплата'"):
+        if check.equal(page.button_dostavka_i_oplata.get_text(), 'Доставка и оплата'):
+            check.equal(page.button_dostavka_i_oplata.get_attribute('href'), 'https://www.dollar.by/orders')
+            page.button_dostavka_i_oplata.click(1)
+            page1 = page.get_current_url()
+            check.equal(page1, 'https://www.dollar.by/orders')
+            check.is_true(page.button_dostavka_i_oplata.wait_for_visibility())
+            check.is_true(page.button_dostavka_i_oplata.wait_to_be_clickable())
+
+    with allure.step("Проверка кнопки 'Гарантия'"):
+        if check.equal(page.button_garantiya.get_text(), 'Гарантия'):
+            check.equal(page.button_garantiya.get_attribute('href'), 'https://www.dollar.by/garantiya')
+            page.button_garantiya.click(1)
+            page1 = page.get_current_url()
+            check.equal(page1, 'https://www.dollar.by/garantiya')
+            check.is_true(page.button_garantiya.wait_for_visibility())
+            check.is_true(page.button_garantiya.wait_to_be_clickable())
+
+    with allure.step("Проверка кнопки 'Корзина'"):
+        if check.equal(page.button_korzina.get_text(), 'Корзина:'):
+            check.equal(page.button_korzina.get_attribute('href'), 'https://www.dollar.by/shopcart')
+            page.button_korzina.click(1)
+            page1 = page.get_current_url()
+            check.equal(page1, 'https://www.dollar.by/shopcart')
+            check.is_true(page.button_korzina.wait_for_visibility())
+            check.is_true(page.button_korzina.wait_to_be_clickable())
+
+    with allure.step("Проверка кнопки 'Лого'"):
+        if check.equal(page.button_logo.get_attribute('href'), 'https://www.dollar.by/'):
+            page.button_logo.click(1)
+            page1 = page.get_current_url()
+            check.equal(page1, 'https://www.dollar.by/')
+            check.is_true(page.button_logo.wait_for_visibility())
+            check.is_true(page.button_logo.wait_to_be_clickable())
+
+    with allure.step("Проверка кнопки 'Поиск'"):
+        if check.equal(page.button_poisk.get_text(), 'Поиск'):
+            page.button_poisk.click(1)
+            page1 = page.get_current_url()
+            check.equal(page1, 'https://www.dollar.by/search?q=')
+            check.is_true(page.button_poisk.wait_for_visibility())
+            check.is_true(page.button_poisk.wait_to_be_clickable())
+            time.sleep(4)
+
+    with allure.step("Проверка кнопки Header-mini 'Лого'"):
+        page.execute_script("window.scrollTo(0, 200)")
+        time.sleep(4)
+        if check.equal(page.button_logo_mini.get_attribute('href'), 'https://www.dollar.by/'):
+            check.is_true(page.button_logo_mini.wait_for_visibility())
+            check.is_true(page.button_logo_mini.wait_to_be_clickable())
+            page.button_logo_mini.click(1)
+            page1 = page.get_current_url()
+            check.equal(page1, 'https://www.dollar.by/')
+
+    with allure.step("Проверка кнопки Header-mini 'Поиск'"):
+        page.execute_script("window.scrollTo(0, 200)")
+        if check.equal(page.button_poisk_mini.get_text(), 'Поиск'):
+            check.is_true(page.button_poisk_mini.wait_for_visibility())
+            check.is_true(page.button_poisk_mini.wait_to_be_clickable())
+            page.button_poisk_mini.click(1)
+            page1 = page.get_current_url()
+            check.equal(page1, 'https://www.dollar.by/search?q=')
+
+    with allure.step("Проверка кнопки Header-mini 'Корзина'"):
+        page.execute_script("window.scrollTo(0, 200)")
+        if check.equal(page.button_korzina_mini.get_attribute('href'), 'https://www.dollar.by/shopcart'):
+            check.equal(page.button_korzina_mini.get_text(), 'Корзина:')
+            check.is_true(page.button_korzina_mini.wait_for_visibility())
+            check.is_true(page.button_korzina_mini.wait_to_be_clickable())
+            page.button_korzina_mini.click(1)
+            page1 = page.get_current_url()
+            check.equal(page1, 'https://www.dollar.by/shopcart')
+
+
+@allure.feature('Смоук тест')
+@allure.story('Имитация скролла в низ экрана и нажатие кнопоки Инстаграм')
+def test_header_btn_2(web_browser):
+    """ Убеждаемся, что ссылка/кнопка Инстаграм
+        кликабельна и переход на страницу корректный. """
+
+    page = HeaderBtn(web_browser)
 
     with allure.step("Проверка кнопки 'Инстаграм'"):
         if check.equal(page.button_instagram.get_attribute('href'), 'https://www.instagram.com/dollar_by'):
             check.is_true(page.button_instagram.wait_for_visibility())
             check.is_true(page.button_instagram.wait_to_be_clickable())
-            page.button_instagram.scroll_mouse_click(1)
-            page.switch_to_window(web_browser.window_handles[1])
+            page.button_instagram.click(1)
             page1 = page.get_current_url()
-            check.equal(page1, 'https://www.instagram.com/dollar_by/')
+            check.equal(page1, 'https://www.instagram.com/dollar_by')
 
+
+@allure.feature('Смоук тест')
+@allure.story('Имитация скролла в низ экрана и нажатие кнопоки Фейсбук')
+def test_header_btn_3(web_browser):
+    """ Убеждаемся, что ссылка/кнопка Фейсбук
+        кликабельна и переход на страницу корректный. """
+
+    page = HeaderBtn(web_browser)
     with allure.step("Проверка кнопки 'Фейсбук'"):
         if check.equal(page.button_facebook.get_attribute('href'), 'https://web.facebook.com/shop.dollarby'):
             check.is_true(page.button_facebook.wait_for_visibility())
@@ -57,3 +128,60 @@ def test_header_btn_1(web_browser):
             page.button_facebook.click(1)
             page1 = page.get_current_url()
             check.equal(page1, 'https://www.facebook.com/shop.dollarby?_rdc=1&_rdr')
+            time.sleep(3)
+
+
+@allure.feature('Смоук тест')
+@allure.story('Имитация скролла в низ экрана и нажатие кнопоки Телеграм')
+def test_header_btn_4(web_browser):
+    """ Убеждаемся, что ссылка/кнопка Телеграм
+        кликабельна и переход на страницу корректный. """
+
+    page = HeaderBtn(web_browser)
+    with allure.step("Проверка кнопки 'Телеграм'"):
+        if check.equal(page.button_telegram.get_attribute('href'), 'https://t.me/dollar_by'):
+            check.is_true(page.button_telegram.wait_for_visibility())
+            check.is_true(page.button_telegram.wait_to_be_clickable())
+            page.button_telegram.click(1)
+            page.switch_to_window(1)
+            page1 = page.get_current_url()
+            check.equal(page1, 'https://t.me/dollar_by')
+
+
+@allure.feature('Смоук тест')
+@allure.story('Имитация скролла в низ экрана и нажатие кнопоки Вайбер')
+def test_header_btn_5(web_browser):
+    """ Убеждаемся, что ссылка/кнопка Вайбер
+        кликабельна и переход на страницу корректный. """
+
+    page = HeaderBtn(web_browser)
+    with allure.step("Проверка кнопки 'Вайбер'"):
+        if check.equal(page.button_viber.get_attribute('href'), 'https://invite.viber.com/'
+                                                                '?g2=AQBIWbyAVgCbg0%2B1SCjEk6'
+                                                                'Bag5eggt%2FlNWuDPWLjmgnpuFeM'
+                                                                'EH5DJmQf93uWJwJK'):
+            check.is_true(page.button_viber.wait_for_visibility())
+            check.is_true(page.button_viber.wait_to_be_clickable())
+            page.button_viber.click(1)
+            page.switch_to_window(1)
+            page1 = page.get_current_url()
+            check.equal(page1, 'https://invite.viber.com/?g2=AQBIWbyAVgCbg0%2B'
+                               '1SCjEk6Bag5eggt%2FlNWuDPWLjmgnpuFeMEH5DJmQf93uWJwJK&lang=ru')
+
+
+@allure.feature('Смоук тест')
+@allure.story('Имитация скролла в низ экрана и нажатие кнопоки Ватцап')
+def test_header_btn_6(web_browser):
+    """ Убеждаемся, что ссылка/кнопка Ватцап
+        кликабельна и переход на страницу корректный. """
+
+    page = HeaderBtn(web_browser)
+    with allure.step("Проверка кнопки 'Ватцап'"):
+        if check.equal(page.button_watsapp.get_attribute('href'), 'https://wa.me/+375296041133'):
+            check.is_true(page.button_watsapp.wait_for_visibility())
+            check.is_true(page.button_watsapp.wait_to_be_clickable())
+            page.button_watsapp.click(1)
+            page.switch_to_window(1)
+            page1 = page.get_current_url()
+            check.equal(page1, 'https://api.whatsapp.com/send/?phone=%2B375296041133&text&type'
+                               '=phone_number&app_absent=0')
